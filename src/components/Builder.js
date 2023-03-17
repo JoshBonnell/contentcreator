e83357f2c0a0dba273d073b188685fc44dbe4ded
Josh B <joshrbonnell@gmail.com>
2023-03-17 16:35:09 -0500

    EXTREMELY basic editing capability

--- src/components/Builder.js
+++ src/components/Builder.js
@@ -1,19 +1,43 @@
 import "../css/App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CustomGrid from "./CustomGrid";
 import { data } from "../data";
 import BuilderElementsMenu from "./BuilderElementsMenu";
+import ItemEditor from "./ItemEditor";
 
 const PageBuilder = () => {
   const [items, setItems] = useState(data.content.body);
 
+  const [itemToEdit, setItemToEdit] = useState(null);
+
+  const handleGridItemClick = (item) => {
+    setItemToEdit(item);
+  };
+
+  const handleSaveChanges = (updatedItem) => {
+    console.log(updatedItem);
+    setItems(
+      items.map((item) => (item._uid === updatedItem._uid ? updatedItem : item))
+    );
+
+    setItemToEdit(null);
+  };
+
   return (
     <div className="builder">
       <div className="sidebar">
-        <BuilderElementsMenu />
+        {itemToEdit !== null ? (
+          <ItemEditor item={itemToEdit} onSaveChanges={handleSaveChanges} />
+        ) : (
+          <BuilderElementsMenu />
+        )}
       </div>
       <div className="main">
-        <CustomGrid items={items} setItems={setItems} />
+        <CustomGrid
+          items={items}
+          setItems={setItems}
+          onGridItemClick={handleGridItemClick}
+        />
       </div>
     </div>
   );
---
+++ src/components/ItemEditor.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+const ItemEditor = (props) => {
+  const [item, setItem] = useState(props.item);
+
+  // useEffect(() => {
+  //   if (item) {
+  //     console.log(item);
+  //   }
+  // }, []);
+
+  const handleTextChange = (e) => {
+    // console.log( e.target.value );
+    let temp = { ...props.item };
+    temp.props.text = e.target.value;
+    setItem(temp);
+  };
+
+  return (
+    <div
+      className="item-editor"
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      <input type="text" onChange={handleTextChange} value={item.props.text} />
+      <button onClick={() => props.onSaveChanges(item)}>SAVE</button>
+    </div>
+  );
+};
+
+export default ItemEditor;
