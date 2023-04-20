33bdd2c0409d2414cd4bc105efbb8cef352b9475
jobo-svi <113558796+jobo-svi@users.noreply.github.com>
2023-04-20 16:10:39 -0500

    Updates

---
+++ precache-manifest.0dba1483c2c3f7cc7c8b869bc7212de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a41a2fc5c6e24523fc1",
+    "url": "/contentcreator/static/css/main.2e6ddac4.chunk.css"
+  },
+  {
+    "revision": "4a41a2fc5c6e24523fc1",
+    "url": "/contentcreator/static/js/main.4a41a2fc.chunk.js"
+  },
+  {
+    "revision": "97bc1625e33b04eb7828",
+    "url": "/contentcreator/static/js/1.97bc1625.chunk.js"
+  },
+  {
+    "revision": "dfec4f3aede66212f2d7",
+    "url": "/contentcreator/static/js/2.dfec4f3a.chunk.js"
+  },
+  {
+    "revision": "700e2c778f3cfbf151df",
+    "url": "/contentcreator/static/js/runtime~main.700e2c77.js"
+  },
+  {
+    "revision": "caf2cd85c7ea4cdd92556862d5f56314",
+    "url": "/contentcreator/index.html"
+  }
+];
